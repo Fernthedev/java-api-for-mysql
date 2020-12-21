bcf4c21b4d7cebb518167626608f64f7fb215187
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2020-12-21 10:50:03 -0400

    Add data types.

---
+++ src/main/java/org/panteleyev/mysqlapi/DataTypes.java
@@ -0,0 +1,32 @@
+package org.panteleyev.mysqlapi;
+
+/*
+ * Copyright (c) Petr Panteleyev. All rights reserved.
+ * Licensed under the BSD license. See LICENSE file in the project root for full license information.
+ */
+
+import java.util.*;
+
+public interface DataTypes {
+    String TYPE_BIG_DECIMAL = "java.math.BigDecimal";
+    String TYPE_DATE        = "java.util.Date";
+    String TYPE_LOCAL_DATE  = "java.time.LocalDate";
+    String TYPE_LONG        = "java.lang.Long";
+    String TYPE_INTEGER     = "java.lang.Integer";
+    String TYPE_BOOLEAN     = "java.lang.Boolean";
+    String TYPE_STRING      = "java.lang.String";
+    String TYPE_UUID        = "java.util.UUID";
+    String TYPE_LONG_PRIM   = "long";
+    String TYPE_INT         = "int";
+    String TYPE_BOOL        = "boolean";
+    String TYPE_BYTE_ARRAY  = "byte[]";
+
+    String TYPE_ENUM        = "*** enum ***";
+
+    // Exception strings
+    String CLASS_NOT_ANNOTATED = "Class is not properly annotated: ";
+    String FIELD_NOT_ANNOTATED = "Field is not properly annotated: ";
+    String BAD_FIELD_TYPE   = "Unsupported field type: ";
+
+    Set<String> AUTO_INCREMENT_TYPES = new HashSet<>(Arrays.asList(TYPE_INT, TYPE_INTEGER, TYPE_LONG, TYPE_LONG_PRIM));
+}
